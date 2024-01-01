b8ff9f42630ed0589d43c173cb55b92d917828cb
oosawa-shingo <141128249+oosawa-shingo@users.noreply.github.com>
2024-01-01 16:17:38 +0000

    [update]レイアウト追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,14 +1,6 @@
 class Public::HomesController < ApplicationController
+  before_action :authenticate_end_user!, except: [:top]
   def top
     @post_plamos = PostPlamo.all
   end
-
-  # def guest_sign_in
-  #   user = EndUser.find_or_create_by!(email: 'guest@example.com') do |user|
-  #     user.password = SecureRandom.urlsafe_base64
-  #     user.name = "ゲスト"
-  #   end
-  #   sign_in user
-  #   redirect_to root_path
-  # end
 end
