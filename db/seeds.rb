3b503097820b61ff091a18fb0bc97dda47665a27
oosawa-shingo <141128249+oosawa-shingo@users.noreply.github.com>
2024-01-10 15:36:38 +0000

    [fix]seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: 'aaa@aaa',
-  password: "aaaaaa"
+  email: 'admin@example.com',
+  password: "admin"
 )
 Tag.create([
   { tag_name: '#ガンプラ' },
