810eecf1c02ad2d92b04d3099ab3995056a9761c
oosawa-shingo <141128249+oosawa-shingo@users.noreply.github.com>
2024-05-11 13:47:22 +0000

    プロフィール画像表示お試しコード再修正19

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -14,7 +14,8 @@ class EndUser < ApplicationRecord
     unless profile_image.attached?
       # file_path = Rails.root.join('app/assets/images/no_image2.jpeg')
       # file_path = Rails.root.join('app/public/assets/images/no_image2.jpeg')
-      profile_image.attach(io: File.open('/public/assets/images/no_image2.jpeg'), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      # profile_image.attach(io: File.open('/public/assets/images/no_image2.jpeg'), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(asset_path('/no_image2.jpeg')), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image
   end
